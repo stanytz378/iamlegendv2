bbc0801a9279b3addb0f0b6f3d2ed398c16eb764
stanytz378 <stanytz378@gmail.com>
2026-03-21 03:47:21 +0300

    Update gcadd.js

--- plugins/gcadd.js
+++ plugins/gcadd.js
@@ -1,17 +1,18 @@
-/*****************************************************************************
+ /*****************************************************************************
  *                                                                           *
- *                     Developed By Qasim Ali                                *
+ *                     Developed By STANY TZ                                 *
  *                                                                           *
- *  🌐  GitHub   : https://github.com/GlobalTechInfo                         *
- *  ▶️  YouTube  : https://youtube.com/@GlobalTechInfo                       *
- *  💬  WhatsApp : https://whatsapp.com/channel/0029VagJIAr3bbVBCpEkAM07     *
+ *  🌐  GitHub   : https://github.com/Stanytz378                             *
+ *  ▶️  YouTube  : https://youtube.com/@STANYTZ                              *
+ *  💬  WhatsApp : https://whatsapp.com/channel/0029Vb7fzu4EwEjmsD4Tzs1p     *
  *                                                                           *
- *    © 2026 GlobalTechInfo. All rights reserved.                            *
+ *    © 2026 STANY TZ. All rights reserved.                                 *
  *                                                                           *
- *    Description: This file is part of the MEGA-MD Project.                 *
- *                 Unauthorized copying or distribution is prohibited.       *
+ *    Description: This file is part of the ᴵ ᴬᴹ ᴸᴱᴳᴱᴺᴰ Project.             *
+ *                 Unauthorized copying or distribution is prohibited.      *
  *                                                                           *
- *****************************************************************************/
+ ***************************************************************************/
+
 export default {
     command: 'add',
     aliases: ['invite', 'gcadd', 'addgc'],
